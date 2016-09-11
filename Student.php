7cdfed288e13e47d5e617a99692b6e65716db0e9
j <j@j-PC>
2016-09-11 15:15:02 -0700

    Updated student object comment

--- Student.php
+++ Student.php
@@ -1,23 +1,18 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * This object is used to store records of students.
  *
  * @author j
  */
 class Student {
+    
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
+    
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
@@ -25,12 +20,14 @@ class Student {
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
+    
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
             $total += $value;
         return $total / count($this->grades);
     }
+    
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
